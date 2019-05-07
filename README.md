ae92de7f4f1c0fe2e53a0b5837f9970e78180fa9
argenis0409 <argenis04_09@icloud.com>
2019-05-07 16:25:55 -0700

    change calculator project

---
+++ README.md
@@ -0,0 +1,8 @@
+change calculator.
+
+To get the project started you can install the project files into your command line or code editor.
+Once you do that go ahead and type npm start, this command will run the project locally.
+
+Go ahead and type Amount Due and Amount Received, once you click calculate you will see the exact change you have to give back.
+
+note: An alert will pop if you click the calculate button and the input bars are empty.
--- main.js
+++ main.js
@@ -1,1 +1,49 @@
 // Write your JavaScript here
+function calculateChange(amountReceived, amountDue) {
+    return amountReceived - amountDue;
+
+}
+
+function handleClickEvent(e) {
+    var amountDue = $('#amount-due').val();
+    amountDue = Math.floor(amountDue * 100);
+
+    if (amountDue == "") {
+        alert('please type amount due')
+        return false;
+    }
+
+    var amountReceived = $('#amount-received').val();
+    amountReceived = Math.floor(amountReceived * 100);
+
+    if (amountReceived == "") {
+        alert('please type amount recived')
+        return false;
+    }
+
+    var total = calculateChange(amountReceived, amountDue);
+    var dollars, quarters, dimes, nickels, pennies = 0;
+
+    var cents = total;
+
+    dollars = Math.floor(cents / 100);
+    cents -= dollars * 100;
+    document.getElementById('dollars-output').innerHTML = dollars;
+
+    quarters = Math.floor(cents / 25);
+    cents -= quarters * 25;
+    document.getElementById('quarters-output').innerHTML = quarters;
+
+    dimes = Math.floor(cents / 10);
+    cents -= dimes * 10;
+    document.getElementById('dimes-output').innerHTML = dimes;
+
+    nickels = Math.floor(cents / 5);
+    cents -= nickels * 5;
+    document.getElementById('nickels-output').innerHTML = nickels;
+
+    pennies = Math.floor(cents / 1);
+    document.getElementById('pennies-output').innerHTML = pennies;
+}
+
+$('#calculate-change').click(handleClickEvent);
